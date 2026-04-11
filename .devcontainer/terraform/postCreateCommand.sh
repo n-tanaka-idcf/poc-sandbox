40e3c717f969d102ec3ff08611c31146e3a7fde4
Nobuchika Tanaka <n.tanaka@idcf.jp>
2026-04-11 21:35:23 +0000

    ディレクトリ構成を変更

--- .devcontainer/terraform/postCreateCommand.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Install misc commands
-aqua install --config .devcontainer/${DEVCONTAINER_NAME}/aqua.yaml
-
-# Setup starship config
-mkdir -p ${HOME}/.config
-cp .devcontainer/${DEVCONTAINER_NAME}/starship.toml ${HOME}/.config/starship.toml
---
+++ terraform/postCreateCommand.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Install misc commands
+aqua install --config ${DEVCONTAINER_NAME}/aqua.yaml
+
+# Setup starship config
+mkdir -p ${HOME}/.config
+cp ${DEVCONTAINER_NAME}/starship.toml ${HOME}/.config/starship.toml
